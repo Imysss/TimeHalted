a3c445f0d41b3d686c2998bf80e6ba6d0de1b82a
Im Ye Seul <147143220+Imysss@users.noreply.github.com>
2025-05-02 18:28:09 +0900

    fix: camera position
    - flappy game > doesnt move y axis
    - main game > move y axis

--- TimeHalted/Assets/Scripts/Controllers/CameraController.cs
+++ TimeHalted/Assets/Scripts/Controllers/CameraController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
 
     private float offsetX;
+    private float offsetY;
+
+    private bool isFlappyGame = false;
 
     private void Start()
     {
@@ -14,6 +18,16 @@ public class CameraController : MonoBehaviour
             return;
 
         offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
+
+        if (GameObject.Find("FlappyGameManager") == null)
+        {
+            isFlappyGame = false;
+        }
+        else
+        {
+            isFlappyGame = true;
+        }
     }
 
     private void FixedUpdate()
@@ -22,6 +36,11 @@ public class CameraController : MonoBehaviour
 
         Vector3 pos = transform.position;
         pos.x = target.position.x + offsetX;
+        //FlappyGame을 시작했을 경우에는 y축은 움직이지 않도록 설정
+        if (!isFlappyGame)
+        {
+            pos.y = target.position.y + offsetY;
+        }
         transform.position = pos;
     }
 }
